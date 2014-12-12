2baa6d7ee8575de7ee9898b8ddc68871c11ebc0c
Witek <baranowskiwitek@gmail.com>
2014-12-12 22:10:20 +0100

    version updated to 1.7 first red test for shelf storage

--- src/test/java/tdd/vendingMachine/VendingMachineTest.java
+++ src/test/java/tdd/vendingMachine/VendingMachineTest.java
@@ -1,5 +1,25 @@
 package tdd.vendingMachine;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class VendingMachineTest {
 
+
+
+
+    @Test
+    public void shouldReturnCokeFromShelfIfChosenOne(){
+        String coke = "coke";
+        Map<Integer, String> shelfs = new HashMap<>();
+
+        String returnedProduct = shelfs.get(1);
+
+        assertThat(returnedProduct).isNotNull();
+        assertThat(returnedProduct).isEqualTo(coke);
+
+    }
 }

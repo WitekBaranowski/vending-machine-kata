6c2fb57cec0687cf53c27a26cf8095349e16898e
Witek <baranowskiwitek@gmail.com>
2014-12-12 22:40:02 +0100

    extracted Shelf class, red for tests

---
+++ src/main/java/tdd/vendingMachine/Shelf.java
@@ -0,0 +1,18 @@
+package tdd.vendingMachine;
+
+
+public class Shelf {
+    private String productType;
+
+    public Shelf(String productType) {
+        this.productType = productType;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+}
--- src/test/java/tdd/vendingMachine/VendingMachineTest.java
+++ src/test/java/tdd/vendingMachine/VendingMachineTest.java
@@ -3,7 +3,6 @@ package tdd.vendingMachine;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,37 +13,44 @@ public class VendingMachineTest {
     static String CHOCOLATE_BAR = "chocolate bar";
     static String COKE = "coke";
 
+    private Map<Integer, Shelf> shelfs;
 
+    @Before
+    public void setUp() throws Exception {
+        shelfs = new HashMap<>();
+        prepareShelfs(shelfs);
+
+    }
 
     @Test
     public void shouldReturnCokeFromShelfIfChosenFirstShelf(){
 
-        Map<Integer, String> shelfs = prepareShelfs();
 
-        String returnedProduct = shelfs.get(1);
 
-        assertThat(returnedProduct).isNotNull();
-        assertThat(returnedProduct).isEqualTo(COKE);
+        Shelf returnedShelf = shelfs.get(1);
+
+        assertThat(returnedShelf).isNotNull();
+        assertThat(returnedShelf).isEqualTo(COKE);
 
     }
     @Test
     public void shouldReturnChocolateBarFromShelfIfChosenSecondShelf(){
-        Map<Integer, String> shelfs = prepareShelfs();
 
-        String returnedProduct = shelfs.get(2);
 
-        assertThat(returnedProduct).isNotNull();
-        assertThat(returnedProduct).isEqualTo(CHOCOLATE_BAR);
+        Shelf returnedShelf = shelfs.get(2);
+
+        assertThat(returnedShelf).isNotNull();
+        assertThat(returnedShelf).isEqualTo(CHOCOLATE_BAR);
     }
 
 
 
 
-    private Map<Integer, String> prepareShelfs() {
-        Map<Integer, String> shelfs = new HashMap<>();
-        shelfs.put(1, CHOCOLATE_BAR);
-        shelfs.put(2, COKE);
-        return shelfs;
+
+    private void prepareShelfs(Map<Integer, Shelf> shelfs) {
+        shelfs.put(1, new Shelf(CHOCOLATE_BAR));
+        shelfs.put(2, new Shelf(COKE));
+
     }
 
 

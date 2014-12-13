6427a1c3903aaaf1598ce4cf7cfe2dbc906911f7
Witek <baranowskiwitek@gmail.com>
2014-12-13 15:03:04 +0100

    adding product price to product information red

---
+++ src/main/java/tdd/vendingMachine/ProductType.java
@@ -0,0 +1,17 @@
+package tdd.vendingMachine;
+
+/**
+ * Created by Witek on 2014-12-13.
+ */
+public class ProductType {
+
+    private long productPrice;
+
+    public ProductType(String coke, long productPrice) {
+
+    }
+
+    public long getProductPrice() {
+        return productPrice;
+    }
+}
--- src/test/java/tdd/vendingMachine/VendingMachineTest.java
+++ src/test/java/tdd/vendingMachine/VendingMachineTest.java
@@ -1,11 +1,25 @@
 package tdd.vendingMachine;
 
 
+import static org.assertj.core.api.Assertions.*;
+import org.junit.Test;
 
+import static tdd.vendingMachine.TestDataConstants.*;
 
 public class VendingMachineTest {
 
 
+    @Test
+    public void testProductTypePrice(){
+        long productPrice = 200;
+        ProductType productType = new ProductType(COKE, productPrice);
 
+
+        long price = productType.getProductPrice();
+
+        assertThat(price).isEqualTo(200);
+
+
+    }
 
 }

b53111ed590482ba7e405165964226f1b156b651
Witek <baranowskiwitek@gmail.com>
2014-12-13 13:10:59 +0100

    storage initial green test, mounting new shelf in storage

--- src/main/java/tdd/vendingMachine/Storage.java
+++ src/main/java/tdd/vendingMachine/Storage.java
@@ -1,19 +1,24 @@
 package tdd.vendingMachine;
 
 
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Witek on 2014-12-13.
  */
 public class Storage {
+    private Map<Integer, Shelf> shelfs;
+
 
 
     public void mountNewShelfInStorage() {
-
+        shelfs= new HashMap<>();
+        shelfs.put(0, new Shelf("default"));
 
     }
 
     public int getNumberOfShelfs() {
-        return 0;
+        return shelfs.size();
     }
 }

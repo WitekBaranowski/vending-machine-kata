77c54233ddc5681c65dcf6b6ebd8204a5f228203
Witek <baranowskiwitek@gmail.com>
2014-12-13 16:03:11 +0100

    test for displaying product price red

--- src/main/java/tdd/vendingMachine/VendingMachine.java
+++ src/main/java/tdd/vendingMachine/VendingMachine.java
@@ -2,4 +2,11 @@ package tdd.vendingMachine;
 
 public class VendingMachine {
 
+    public void enterShelfNumber(int shelfNumber) {
+
+    }
+
+    public String showDisplay() {
+        return null;
+    }
 }
--- src/test/java/tdd/vendingMachine/VendingMachineTest.java
+++ src/test/java/tdd/vendingMachine/VendingMachineTest.java
@@ -8,6 +8,17 @@ import static tdd.vendingMachine.TestDataConstants.*;
 
 public class VendingMachineTest {
 
+    @Test
+    public void testDisplayingProductPriceOnVendingMachine(){
+        VendingMachine vendingMachine = new VendingMachine();
+
+        vendingMachine.enterShelfNumber(2);
+        String display = vendingMachine.showDisplay();
+
+        assertThat(display).isEqualTo("1,2 zł");
+
+    }
+
 
 
 

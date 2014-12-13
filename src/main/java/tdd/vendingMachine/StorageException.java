e5696691e1a6ad69f4d8deb6da712ac463f8af0f
Witek <baranowskiwitek@gmail.com>
2014-12-13 13:52:09 +0100

    test on exception when you put 2 shelf on same slot red

---
+++ src/main/java/tdd/vendingMachine/StorageException.java
@@ -0,0 +1,7 @@
+package tdd.vendingMachine;
+
+/**
+ * Created by Witek on 2014-12-13.
+ */
+public class StorageException extends RuntimeException{
+}
--- src/test/java/tdd/vendingMachine/StorageTest.java
+++ src/test/java/tdd/vendingMachine/StorageTest.java
@@ -3,16 +3,18 @@ package tdd.vendingMachine;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static tdd.vendingMachine.TestDataConstants.*;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class StorageTest {
 
-
-
-
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
 
     @Test
-    public void testAddingNewShelfToStorageOnPosition(){
+    public void testAddingNewShelfToStorageOnGivenPosition(){
         Storage storage = new Storage();
         Shelf shelfToMount = new Shelf(CHOCOLATE_BAR);
 
@@ -20,6 +22,20 @@ public class StorageTest {
 
         assertThat(storage.getNumberOfShelfs()).isEqualTo(1);
         assertThat(storage.listAllAvailableShelfs()).contains(shelfToMount);
+    }
+
+    @Test
+    public void testStorageExceptionIsThrownWhenAddingTwoShelfsOnSamePosition(){
+        Storage storage = new Storage();
+        Shelf chocoShelfToMount = new Shelf(CHOCOLATE_BAR);
+        Shelf cokeShelfToMount = new Shelf(COKE);
+
+
+        storage.mountNewShelfInStorage(CHOCOLATE_BAR_SHELF_NUMBER, chocoShelfToMount);
+        storage.mountNewShelfInStorage(CHOCOLATE_BAR_SHELF_NUMBER, cokeShelfToMount);
+
+        exception.expect(StorageException.class);
+        exception.expectMessage("There is already mounted shelf at position:");
     }
 
     
